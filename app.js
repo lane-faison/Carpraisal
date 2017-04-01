f8cdfb3b5c4eb880a5666f684e7675043865640d
lanefaison2 <faisonusmc@gmail.com>
2017-04-01 08:39:09 -0600

    1st commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const path = require('path')
+// const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const index = require('./routes/index')
+const user = require('./routes/user')
+require('dotenv').config()
+const passport = require('passport')
+const cors = require('cors')
+const app = express()
+
+// view engine setup
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.use(express.static(path.join(__dirname, 'public')))
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  credentials: true
+}))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(session({
+  secret: 'shhhhhhhhh',
+  saveUninitialized: true,
+  resave: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', index)
+app.use('/user', user)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.send(err.message)
+})
+
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var router = express.Router()
+require('dotenv').config()
+var passport = require('../passport')
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send('hello')
+})
+
+router.get('/login',
+  function(req, res){
+    res.render('login', { env: env })
+})
+
+router.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/')
+})
+
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: 'http://www.google.com' }),
+  function(req, res) {
+    console.log(req.user._json.email)
+    const userEmail = req.user._json.email
+    res.redirect('http://localhost:3000/user.html')
+})
+
+
+module.exports = router
